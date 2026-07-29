8305779cfd8e792ec27d71b57c3a303aa71a3f41
kyledls <kad387@scarletmail.rutgers.edu>
2026-07-29 01:08:56 -0700

    Add: python-multiple-parameters - submission-0

---
+++ Python For Beginners/python-multiple-parameters/submission-0.py
@@ -0,0 +1,14 @@
+def two_sum(num_1, num_2):
+    sum_1 = num_1 + num_2
+    print(sum_1)
+
+def three_sum(num_1, num_2, num_3):
+    sum_1 = num_1 + num_2 + num_3
+    print(sum_1)
+
+two_sum(7, 10)
+three_sum(3, 5, 8)
+
+# do not modify below this line
+two_sum(10, 9)
+three_sum(5, 14, 6)
