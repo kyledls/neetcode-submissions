ae56d2cb25ce7da4e0d98292384bfe0fde4bc764
Kyle <kad387@scarletmail.rutgers.edu>
2026-08-04 13:47:36 -0400

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        sort_s = sorted(s)
+        sort_t = sorted(t)
+
+        if sort_s == sort_t:
+            return True
+        else:
+            return False
